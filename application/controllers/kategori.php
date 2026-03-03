5a38ae7416d4350bf8da19bbfd5e214600194cc7
maylita14 <maylitasumarnah@gmail.com>
2026-03-03 14:34:27 +0700

    Initial commit

---
+++ application/controllers/kategori.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH')OR exit('No direct script access allowed');
+
+class kategori extends CI_Controller{
+
+    public function __construct()
+    {
+        parent ::__construct();
+        $this->load->model('Kategori_model');
+
+    }
+    public function index()
+    {
+        $data['kategori'] = $this->Kategori_model->get_all();
+        $this->load->view('kategori/index', $data);
+
+    }
+    public function tambah()
+    {
+        $this->load->view('kategori/tambah');
+    }
+    public function simpan()
+    {
+        $data= [
+            'nama_kategori'=> $this->input->post('nama_kategori')
+        ];
+
+    $this->Kategori_model->insert($data);
+    redirect('kategori');
+    }
+}
