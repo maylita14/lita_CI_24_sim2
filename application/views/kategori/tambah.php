2d2671ef9195c55dc6cb0db358d80860ad8351aa
maylita14 <maylitasumarnah@gmail.com>
2026-03-03 15:50:21 +0700

    update template

--- application/views/kategori/tambah.php
+++ application/views/kategori/tambah.php
@@ -2,7 +2,7 @@
 <a href="<?= site_url('kategori'); ?> ">Kembali</a>
 <br><br>
 
-<form method="post" action="<? site_url('kategori/simpan'); ?>">
+<form method="post" action="<?= site_url('kategori/simpan'); ?>">
 
     <label>Nama Kategori</label><br>
     <input type="text" name="nama_kategori" required>
